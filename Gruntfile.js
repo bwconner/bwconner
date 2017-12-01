f17a881781ef419e34a69cdb377e14e11d2045ef
Bruce Conner <bruce.conner.ii@gmail.com>
2017-11-30 19:40:14 -0500

    Comic Collector Subdomain Added
    Adding in initial folder and file structure for the comic collector
    project that will be a subdomain for the time being. Updating gruntfile
    to sass these styles.

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,12 @@ module.exports = function(grunt) {
           'bwconner/css/contact.css' : 'bwconner/scss/contact.scss',
           'bwconner/css/index.css' : 'bwconner/scss/index.scss',
           'bwconner/css/media.css' : 'bwconner/scss/media.scss',
-          'bwconner/css/projects.css' : 'bwconner/scss/projects.scss'
+          'bwconner/css/projects.css' : 'bwconner/scss/projects.scss',
+          'comicCollector/css/base.css' : 'comicCollector/scss/base.scss',
+          'comicCollector/css/index.css' : 'comicCollector/scss/index.scss',
+          'comicCollector/css/search.css' : 'comicCollector/scss/search.scss',
+          'comicCollector/css/profile.css' : 'comicCollector/scss/profile.scss',
+          'comicCollector/css/comicInfoPage.css' : 'comicCollector/scss/comicInfoPage.scss'
         }
       }
     },
