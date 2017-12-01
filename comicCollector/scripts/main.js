9acf978e70aa662a60ae3711a48b97cc2329ceb0
Bruce Conner <bruce.conner.ii@gmail.com>
2017-11-30 21:41:57 -0500

    Comic Collector Style Updates
    Lot of base and homepage style updates. Recoloring fonts and background.
    Getting basic styles in general in place. Adding in mininav as well.

--- comicCollector/scripts/main.js
+++ comicCollector/scripts/main.js
@@ -1,139 +1,22 @@
-var resultLimit = 25;
-var currentFormat = "";
-var currentOffset = 0;
-var currentPage = 1;
-var currentSearch;
+function miniNavSetup() {
+	var headerHeight = $('.header').height(),
+		$window = $(window);
 
-function getResults(searchTerm) {
-	$(".results").html(""); //empty results section
-	currentSearch = searchTerm;
-	var apiUrl = marvelApi.buildApiUrl(searchTerm, resultLimit, currentOffset);
-	$.get(apiUrl, function( data ) {
-		processResults(data);
-	});
-}
-
-function reloadResults() {
-	$(".results").html(""); //empty results section
-	var apiUrl = marvelApi.buildApiUrl(searchTerm, resultLimit, currentOffset);
-	$.get(apiUrl, function( data ) {
-		processResults(data);
-	});
-}
-
-function processResults(data) {
-	var totalResults = data.data.total;
-	var resultsList = data.data.results;
-	var totalPages = Math.ceil(totalResults/resultLimit);
-
-	//console.log(totalResults);
-	//console.log(totalPages);
-	console.log(resultsList);
-	//outputResults(resultsList);
-
-	$(resultsList).each(function(result) {
-		var resultData = $(this);
-		if (resultData[0].images[0] !== undefined && resultData[0].images[0].path !== undefined) {
-			var resultImage = resultData[0].images[0].path + "." + resultData[0].images[0].extension;
-		} else {
-			//no image available
-			var resultImage = "http://bwconner.com/bwconner/images/noimage.jpg";
+	//show mini nav
+	$window.scroll(function() {
+		if ($window.scrollTop() > headerHeight ) {
+			$(".mini-header").addClass("in-view");
 		}
-
-		var resultTitle = resultData[0].title;
-		var resultSeries = resultData[0].series.name;
-		var resultIssueNumber = resultData[0].issueNumber;
-		var resultDescription = resultData[0].description;
-	
-		var featuredCharacters = [];
-		var resultCreators = [];
-
-		$(resultData[0].characters.items).each(function(index) {
-			featuredCharacters[index] = $(this)[0].name;
-		});
-
-		$(resultData[0].creators.items).each(function(index) {
-			resultCreators[index] = $(this)[0].name + " - " + $(this)[0].role;
-		});
-
-		var markup = buildResultMarkup(resultTitle, resultImage, resultSeries, resultIssueNumber, resultDescription, featuredCharacters, resultCreators);
-
-
-		$(".results").append(markup);
-	});
-}
-
-function outputResults(resultsList) {
-	$(resultsList).each(function(result) {
-		var resultData = $(this);
-		var resultImage = resultData[0].images[0].path + "." + resultData[0].images[0].extension;
-		var resultTitle = resultData[0].title;
-		var markup = buildResultMarkup(resultTitle, resultImage);
-
-
-		$(".results").append(markup);
 	});
 
-}
-
-function buildResultMarkup(resultTitle, resultImage, resultSeries, resultIssueNumber, resultDescription, featuredCharacters, resultCreators) {
-		var markup = "<div class='search-result'>" +
-		"<img src='" + resultImage + "'/>" +
-		"<div class='result-information'>" +
-		"<div class='result-title'>" + resultTitle + "</div>";
-
-		//sanitize data function
-		if(resultCreators !== null) {
-			markup = markup + "<div class='result-creators'> Creators: " + resultCreators + "</div>";
+	//show full nav
+	$window.scroll(function() {
+		if ( $window.scrollTop() <= headerHeight ) {
+			$(".mini-header").removeClass("in-view");
 		}
-
-		if(resultDescription !== null) {
-			markup = markup + "<div class='result-description'>" + resultDescription + "</div>";
-		}
-
-		if(featuredCharacters !== null) {
-			markup = markup + "<div class='result-characters'> Featured Characters: " + featuredCharacters + "</div>";
-		}
-
-		markup = markup + "</div></div>";
-		
-
-		return markup;
-}
-
-function gotoPage(newPage) {
-	currentOffset = newPage * resultLimit
-	reloadResults();
+	});
 }
 
 $(document).ready(function() {
-
-	$('.search-limit-option').on('click', function() {
-		resultLimit = $(this).attr("data-limit");
-		console.log(resultLimit);
-	});
-
-	$('.advanced-search-options-show').on('click', function() {
-		$(".advanced-search-options-wrapper").toggleClass("show");
-	});
-
-	$('.page-selector').on('click', function() {
-		gotoPage($(this).attr("data-page"));
-	});
-
-	$('.prev').on('click', function() {
-		gotoPage(currentPage - 1);
-	});
-
-	$('.next').on('click', function() {
-		gotoPage(currentPage + 1);
-	});
-
-	$('.search').on('click', function() {
-		//resultLimit = $("select.search-limit:first").text();
-		//currentFormat = $("select.format:first").text();
-		getResults($('.search-text').val());
-	});
-
+	miniNavSetup();
 });
-
