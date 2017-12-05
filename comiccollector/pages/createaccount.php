57e3b8194cbc0d2fae6b5a8b804d03c6c25ab9e5
Bruce Conner <bruce.conner.ii@gmail.com>
2017-12-04 21:19:55 -0500

    Create Account Styles
    Updates to create account styles and markup.

--- comiccollector/pages/createaccount.php
+++ comiccollector/pages/createaccount.php
@@ -2,20 +2,47 @@
 	<head>
 		<title>bwconner.com | Comic Collector</title>
 		<?php include '../snippets/global.html';?>
-		<link rel="stylesheet" type="text/css" href="../css/login.css">
+		<link rel="stylesheet" type="text/css" href="../css/createAccount.css">
 		<script type="text/javascript" src="../scripts/login.js"></script>
 	</head>
 	<body lang="en">
 		<?php include '../snippets/header.html';?>
 		<div class="site-body-wrapper">
 			<div class="create-account-wrapper">
-				<form id="form" method="post" action="">
-					<input id="username" type="text" name="username" class="username" placeholder="Enter a User Name"/>
-					<input id="password" type="text" name="password" class="password" placeholder="Enter a Password"/>
-					<input id="firstname" type="text" name="firstname" class="firstname" placeholder="Enter a First Name"/>
-					<input id="email" type="text" name="email" class="email" placeholder="Enter an E-Mail"/>
-				</form>
-				<button class="create-account">Create Account</button>
+				<div class="create-account-wrapper_form">
+					<div class="section-header">Create An Account</div>
+					<form id="form" method="post" action="">
+						<p class="input-label">Username:</p>
+						<input id="username" type="text" name="username" class="username" placeholder="Enter a User Name"/>
+						<p class="error username-error hide">Please enter a username.</p>
+						<p class="input-label">Password:</p>
+						<input id="password" type="text" name="password" class="password" placeholder="Enter a Password"/>
+						<p class="error password-error hide">Please enter a password.</p>
+						<p class="input-label">E-Mail:</p>
+						<input id="email" type="text" name="email" class="email" placeholder="Enter an E-Mail"/>
+						<p class="error email-error hide">Please enter a e-mail.</p>
+					</form>
+					<button class="create-account button">Create Account</button>
+				</div>
+				<div class="create-account-wrapper_benefits">
+					<div class="section-header">Benefits of an account:</div>
+					<div class="create-account-section">
+						<ul>
+							<li>
+								Connect with friends.
+							</li>
+							<li>
+								Manage your collection.
+							</li>
+							<li>
+								Add comics to your wishlist.
+							</li>
+							<li>
+								Discover new reads.
+							</li>
+						</ul>
+					</div>
+				</div>
 			</div>
 		</div>
 	</body>
--- comiccollector/snippets/createaccountsql.php
+++ comiccollector/snippets/createaccountsql.php
@@ -10,8 +10,8 @@
 	//Turn posted attributes into variables
 	$post_username= $_POST["username"];
 	$post_password= $_POST["password"];
-	$post_firstName= $_POST["email"];
-	$post_firstName= $_POST["firstname"];
+	$post_email= $_POST["email"];
+	$post_firstName= '';
 	$post_userID= $_POST["userID"];
 	$post_profileText= "";
 	$post_favoriteCharacter= "";
@@ -20,8 +20,8 @@
 
 	//Insert the new profile into the DB
 	$link = mysqli_connect($hostname,$username, $password, $dbname);
-	$sql = "INSERT user_profiles (userID, userName, password, firstName, favoriteCharacter, profileText, profileImage)
-	        VALUES ('".$post_userID."', '".$post_username."','".$post_password."','".$post_firstName."', '', '', 'http://bwconner.com/comiccollector/images/blankprofile.png')";
+	$sql = "INSERT user_profiles (userID, userName, password, email, firstName, favoriteCharacter, profileText, profileImage)
+	        VALUES ('".$post_userID."', '".$post_username."','".$post_password."','".$post_email."', '', '', '', 'http://bwconner.com/comiccollector/images/blankprofile.png')";
 	mysqli_query($link, $sql);
 	mysqli_close($link);
 ?>
