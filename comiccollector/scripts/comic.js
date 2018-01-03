ee0497b420a8ebb95027a897d1340ee8749dcde3
Bruce Conner <bruce.conner.ii@gmail.com>
2018-01-02 21:57:49 -0500

    ComicJS Updates
    Adding comics to collection updates and cleanup

--- comiccollector/scripts/comic.js
+++ comiccollector/scripts/comic.js
@@ -1,20 +1,13 @@
 function addComicToCollection(comicId) {
-	alert(comicId);
-	var	data = "&userID=" + $("body").attr("data-userid") + "&comicId=" + comicId;
+	var $currentComic = $(".comic-" + comicId),
+		data = "userID=" + $("body").attr("data-userid") + "&comicId=" + comicId;//+ "&comicTitle=" + $currentComic.attr("data-title") + "&comicCharacter=" + $currentComic.attr("data-character") + "&comicDate=" + $currentComic.attr("data-date") + "&comicImage=" + $currentComic.attr("data-image");
 
 	$.ajax({
 		data: data,
 		type: "post",
 		url: "../phpscripts/addcomictocollection.php",
 		success: function(data){
-			$(".first-name").html($(".firstname-field").val());
-			$(".favorite-character").html($(".favorite-character-field").val());
-			$(".user-description").html($(".user-description-field").val());
 
-			$(".profile-information input").addClass("hide");
-			$(".profile-information .submit-edit").addClass("hide");
-			$(".edit-profile").removeClass("hide");
-			$(".profile-information .edit-success").removeClass("hide");
 		}
 	});
 }
