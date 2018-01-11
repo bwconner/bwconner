7ba517880cb644cab26571460a11da592d0c47cf
Bruce Conner <bruce.conner.ii@gmail.com>
2018-01-10 20:39:01 -0500

    Optimizing JS
    Instead of watching for scrolling twice to check which nav to show,
    watch once and check with an if else to display the correct one.

--- bwconner/scripts/main.js
+++ bwconner/scripts/main.js
@@ -6,12 +6,7 @@ function miniNavSetup() {
 	$window.scroll(function() {
 		if ($window.scrollTop() > headerHeight ) {
 			$(".mini-header").addClass("in-view");
-		}
-	});
-
-	//show full nav
-	$window.scroll(function() {
-		if ( $window.scrollTop() <= headerHeight ) {
+		} else if ( $window.scrollTop() <= headerHeight ) {
 			$(".mini-header").removeClass("in-view");
 		}
 	});
