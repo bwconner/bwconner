e641a72bb09b9306ae5241edaccbfbb0040c296a
Bruce Conner <bruce.conner.ii@gmail.com>
2018-01-13 17:19:27 -0500

    Adding Blank Collection/Wishlist Pages
    Adding Blank Collection/Wishlist pages that will house the view full
    collection and wishlist pages in the future.

---
+++ comiccollector/pages/collection.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<title>bwconner.com | Comic Collector</title>
+		<?php include '../snippets/global.html';?>
+		<link rel="stylesheet" type="text/css" href="../css/collection.css">
+		<script type="text/javascript" src="../scripts/collection.js"></script>
+	</head>
+	<body class="collection">
+		<?php include '../snippets/header.html';?>
+		<div class="site-body-wrapper">
+			<p class="">Collection: </p>
+			<div class="profile__collection-preview">
+				<div  class="profile__comic">
+					No comics added yet.
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ comiccollector/pages/wishlist.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<title>bwconner.com | Comic Collector</title>
+		<?php include '../snippets/global.html';?>
+		<link rel="stylesheet" type="text/css" href="../css/collection.css">
+		<script type="text/javascript" src="../scripts/collection.js"></script>
+	</head>
+	<body class="wishlist">
+		<?php include '../snippets/header.html';?>
+		<div class="site-body-wrapper">
+			<p class="">Wishlist: </p>
+			<div class="profile__wishlist-preview">
+				<div class="profile__comic">
+					No comics added yet.
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
