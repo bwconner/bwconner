0d002863972cc42e9330cb1cc86ec6eb8a7d2f5a
Bruce Conner <bruce.conner.ii@gmail.com>
2018-06-12 20:06:14 -0700

    Importing Bio to Homepage
    Importing Bio to Homepage

--- bwconner-react/src/components/home.js
+++ bwconner-react/src/components/home.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
+import Bio from './bio';
 
 export default class Home extends Component {
-
-
-
 	render() {
 		return (
 			<div>
+				<Bio />
 			</div>
 		);
 	}
