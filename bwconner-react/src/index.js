c4e5ba4db575a9595b6dcaddfd1573fd2ed3ea56
Bruce Conner <bruce.conner.ii@gmail.com>
2018-06-12 18:32:21 -0700

    bwconner.com - React conversion
    Creating a subfolder to hold the work of transferring bwconner into
    being a React based site. This commit basically holds stubbed out file
    structure with mostly empty files.

---
+++ bwconner-react/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import App from './components/app';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+	<Provider store={createStoreWithMiddleware(reducers)}>
+		<div>
+			<BrowserRouter >
+				<div>
+					<Header />
+					<Switch>
+						<Route path="/" component={App} />
+					</Switch>
+				</div>
+			</BrowserRouter>
+		</div>
+	</Provider>
+, document.querySelector('.container'));
