b347b7d59edcd8b4bce43b6b55d449bdbe49a6d0
Bruce Conner <bruce.conner.ii@gmail.com>
2018-06-15 22:32:01 -0700

    Adding Project List to Home Page
    Adding Project List to Home Page

--- bwconner-react/src/components/home.js
+++ bwconner-react/src/components/home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Bio from './bio';
+import ProjectList from '../containers/project-list';
 
 export default class Home extends Component {
 	render() {
 		return (
 			<div>
 				<Bio />
+				<ProjectList />
 			</div>
 		);
 	}
