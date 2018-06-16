0f5ec46bcfe83b90a56f0decb62081a4773ea350
Bruce Conner <bruce.conner.ii@gmail.com>
2018-06-15 23:03:12 -0700

    Converting Images to JPEGs
    Converting all images to JPEGs to save on file size, there was no need
    to be using PNGs for these images.

--- bwconner-react/src/reducers/reducer_projects.js
+++ bwconner-react/src/reducers/reducer_projects.js
@@ -1,11 +1,11 @@
 export default function() {
 	return [
 		{projectTitle: 'Bruce Conner', projectDescription: 'The website you are currently on. My personal site, portfolio and dev playground. This is the place where most of my personal web experiments begin.', projectUrl: 'bwconner.com', projectImage: ''},
-		{projectTitle: 'Uber', projectDescription: 'I have been a front end web engineer with Uber since April of 2018.', projectUrl: '', projectImage: '../../images/projectImages/uber.png'},
-		{projectTitle: 'Abercrombie & Fitch', projectDescription: 'I have been a front end web developer with Abercrombie & Fitch since March of 2012.', projectUrl: '', projectImage: '../../images/projectImages/abercrombie.png'},
-		{projectTitle: 'Hollister Co', projectDescription: 'I also work on HollisterCo.com as a web developer with Abercrombie & Fitch.', projectUrl: '', projectImage: '../../images/projectImages/hollister.png'},
-		{projectTitle: 'The Tar Pit Club', projectDescription: 'I have been the lead web developer for this Columbus based start-up clothing brand since November, 2016.', projectUrl: '', projectImage: '../images/projectImages/tarpitlogo.png'},
+		{projectTitle: 'Uber', projectDescription: 'I have been a front end web engineer with Uber since April of 2018.', projectUrl: '', projectImage: '../../images/projectImages/uber.jpg'},
+		{projectTitle: 'Abercrombie & Fitch', projectDescription: 'I have been a front end web developer with Abercrombie & Fitch since March of 2012.', projectUrl: '', projectImage: '../../images/projectImages/abercrombie.jpg'},
+		{projectTitle: 'Hollister Co', projectDescription: 'I also work on HollisterCo.com as a web developer with Abercrombie & Fitch.', projectUrl: '', projectImage: '../../images/projectImages/hollister.jpg'},
+		{projectTitle: 'The Tar Pit Club', projectDescription: 'I have been the lead web developer for this Columbus based start-up clothing brand since November, 2016.', projectUrl: '', projectImage: '../images/projectImages/tarpitlogo.jpg'},
 		{projectTitle: 'Comic Collector', projectDescription: 'This is a passion project that I have begun development on and am currently hosting on a subdomain of this site. Cataloging and documenting my comic collection and creating a social experience around it to compare collections and wishlists with friends is something I have wanted to do for awhile. After finding no satisfactory solution out there I have decided to make one myself.', projectUrl: 'http://bwconner.com/comiccollector/pages/home', projectImage: ''},
-		{projectTitle: 'The History of the League', projectDescription: 'This website is the official record tracking and general history source for our fantasy football league. It is also my tribute to web design of the 1990s. Marquee elements shall live forever on on The History of the League.', projectUrl: 'https://www.thehistoryoftheleague.com/', projectImage: '../images/projectImages/theleague.png'},
+		{projectTitle: 'The History of the League', projectDescription: 'This website is the official record tracking and general history source for our fantasy football league. It is also my tribute to web design of the 1990s. Marquee elements shall live forever on on The History of the League.', projectUrl: 'https://www.thehistoryoftheleague.com/', projectImage: '../images/projectImages/theleague.jpg'},
 	]
 }
