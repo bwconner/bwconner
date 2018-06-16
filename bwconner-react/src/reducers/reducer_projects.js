6fab5458430840aff4541b1baf26cfd8ccb1910c
Bruce Conner <bruce.conner.ii@gmail.com>
2018-06-15 22:52:22 -0700

    Adding Uber Image
    Adding Uber Image for Project section

--- bwconner-react/src/reducers/reducer_projects.js
+++ bwconner-react/src/reducers/reducer_projects.js
@@ -1,7 +1,7 @@
 export default function() {
 	return [
 		{projectTitle: 'Bruce Conner', projectDescription: 'The website you are currently on. My personal site, portfolio and dev playground. This is the place where most of my personal web experiments begin.', projectUrl: 'bwconner.com', projectImage: ''},
-		{projectTitle: 'Uber', projectDescription: 'I have been a front end web engineer with Uber since April of 2018.', projectUrl: '', projectImage: ''},
+		{projectTitle: 'Uber', projectDescription: 'I have been a front end web engineer with Uber since April of 2018.', projectUrl: '', projectImage: '../../images/projectImages/uber.png'},
 		{projectTitle: 'Abercrombie & Fitch', projectDescription: 'I have been a front end web developer with Abercrombie & Fitch since March of 2012.', projectUrl: '', projectImage: '../../images/projectImages/abercrombie.png'},
 		{projectTitle: 'Hollister Co', projectDescription: 'I also work on HollisterCo.com as a web developer with Abercrombie & Fitch.', projectUrl: '', projectImage: '../../images/projectImages/hollister.png'},
 		{projectTitle: 'The Tar Pit Club', projectDescription: 'I have been the lead web developer for this Columbus based start-up clothing brand since November, 2016.', projectUrl: '', projectImage: '../images/projectImages/tarpitlogo.png'},
