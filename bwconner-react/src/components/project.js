7aa44a578193310504293ff39d8bb61de224c10a
Bruce Conner <bruce.conner.ii@gmail.com>
2018-06-15 22:45:00 -0700

    Project Image Display Logic
    Adding in logic to display project image if it has one and if it doesn't
    it use the project title to fill in the same space.

--- bwconner-react/src/components/project.js
+++ bwconner-react/src/components/project.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 
 export default class Project extends Component {
+
+	buildImageMarkup(projectImage) {
+		return <img className="project__image" src={projectImage}/>;
+	}
+
 	render() {
 		const {projectTitle, projectDescription, projectUrl, projectImage} = this.props;
 
+		let imageClass = projectImage ? "project__image" : "project__live-type";
+		let imageOutput = projectImage ?  this.buildImageMarkup(projectImage) : projectTitle;
+
 		return (
 			<div className="project">
-				<a href={projectUrl}><div className="project__live-type">{projectTitle}</div></a>
+				<a href={projectUrl}><div className={imageClass}>{imageOutput}</div></a>
 				<div className="project__details">
 					<a href="http://bwconner.com/comiccollector/pages/home"><div className="project__title">{projectTitle}</div></a>
 					<div className="project__desc">{projectDescription}</div>
